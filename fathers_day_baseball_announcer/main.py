6f3ff484da18159d0693bab8d6a04d0f184ffa76
Corey Schulz <realcoreyschulz@gmail.com>
2020-06-20 21:27:05 -0600

    Add most of the code. Have to wait until the sports start back up again to scrape the daily start time (if any).

---
+++ fathers_day_baseball_announcer/main.py
@@ -0,0 +1,37 @@
+import pause 
+from datetime import datetime
+from pygame import mixer 
+from time import sleep
+## Schedule task to run 1x per day in the morning / early afternoon 
+##  with `chron` on Linux. 
+## Announce task will be executed later that day. 
+
+
+
+def findBaseballGameTime(): 
+    return "21:26"
+
+
+def announceGameStart(baseballGameTime): 
+    print("Got time: " + baseballGameTime)
+    print("Starting announce process.")
+
+    hourMin = baseballGameTime.split(':')
+
+    currentDate = datetime.now() 
+    gameStartDate = currentDate.replace(hour = int(hourMin[0]), minute = int(hourMin[1]), 
+        second = 0, microsecond = 0)
+    
+    pause.until(gameStartDate)
+
+    ## Finally, play audio file. 
+    mixer.init() 
+    mixer.music.load('audio.mp3')
+    mixer.music.play() 
+
+    sleep(20)
+
+
+
+
+announceGameStart(findBaseballGameTime())
